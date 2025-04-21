eaf33272d8710921c458e5f39823cf5e32f2ee9e
Yash Gosavi <yashrajendragosavi111@gmail.com>
2025-04-21 16:22:42 +0530

    Sanitized DBUtil for GitHub

--- DBUtil.java
+++ DBUtil.java
@@ -1,10 +1,7 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 public class DBUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/ExpenseTrackerDB";
-    private static final String USER = "root";
-    private static final String PASSWORD = "YashG111@$"; 
+    private static final String URL = "jdbc:mysql://localhost:3306/your_db_name";
+    private static final String USER = "your_real_username";
+    private static final String PASSWORD = "your_real_password";
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
